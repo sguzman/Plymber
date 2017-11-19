e5c72f1514ce48e3c3dc61ca520f78dd5bf67c5f
Salvador Guzman <guzmansalv@gmail.com>
2017-11-19 03:00:23 -0800

    print out statement list

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import requests
 import sys
+import multiprocessing
+from joblib import Parallel, delayed
 
-
-#cookies = uber.cookie()
 
 cookies = sys.argv[1]
+head = {'Cookie': cookies}
 
 url = 'https://partners.uber.com/p3/money/statements/all_data/'
-r = requests.get(url, headers={'Cookie': cookies})
+r = requests.get(url, headers=head)
+json = r.json()
+uuids = [statement['uuid'] for statement in json]
+statementTripUrl = 'https://partners.uber.com/p3/money/statements/view/'
+
+numCores = multiprocessing.cpu_count()
 
 
-print(r.json())
+def req(uuid):
+    j = requests.get(statementTripUrl + uuid, headers=head).json()
+    print(uuid, j)
+    return j
+
+
+statements = Parallel(n_jobs=16)(delayed(req)(u) for u in uuids)
+
+print(statements)
