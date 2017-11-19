26c69dc38d641f4c6de4e7798e906fb6c47c78ed
Salvador Guzman <guzmansalv@gmail.com>
2017-11-19 03:23:34 -0800

    succeeded in retrieving json of trips

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import sys
-import multiprocessing
 from joblib import Parallel, delayed
 
 
@@ -13,8 +12,6 @@ json = r.json()
 uuids = [statement['uuid'] for statement in json]
 statementTripUrl = 'https://partners.uber.com/p3/money/statements/view/'
 
-numCores = multiprocessing.cpu_count()
-
 
 def req(uuid):
     j = requests.get(statementTripUrl + uuid, headers=head).json()
@@ -22,6 +19,25 @@ def req(uuid):
     return j
 
 
-statements = Parallel(n_jobs=16)(delayed(req)(u) for u in uuids)
+def trips(js):
+    triplist = js['body']['driver']['trip_earnings']['trips'].keys()
+    print(triplist)
+    return triplist
 
-print(statements)
+
+statements = Parallel(n_jobs=16)(delayed(req)(u) for u in uuids)
+tripsList = [trips(j) for j in statements]
+tripy = [k for j in tripsList for k in j]
+
+tripUrl = 'https://partners.uber.com/p3/money/trips/trip_data/'
+
+
+def reqtrip(uuid):
+    j = requests.get(tripUrl + uuid, headers=head).json()
+    print(uuid, j)
+    return j
+
+
+tripBody = Parallel(n_jobs=32)(delayed(reqtrip)(u) for u in tripy)
+
+print(tripBody)
